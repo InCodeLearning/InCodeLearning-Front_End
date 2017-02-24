cc2c6b81d882ad98e83f50d3749f17c6bb4a03c7
Jesse Zhuang <zhzexi@gmail.com>
2017-02-23 22:42:35 -0800

    initial commit, angular1, css, html started

---
+++ angular1/module-directive.js
@@ -0,0 +1,9 @@
+/* a directive is a marker on HTML tag for angular to run or reference 
+javascript code */
+
+/* modules encapsulates pieces of angular application and definitions of 
+dependencies for our app, which makes our code more maintainable, 
+testable, and readable. */
+var app = angular.module('store',[]);
+/* expressions allow one to insert dynamic values into html 
+e.g. {{4+6}} */
